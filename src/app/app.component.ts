63253a68ef23324a45829c96e331f969ad2df8cd
Robert Villarreal <robertv1979@gmail.com>
2017-08-09 17:22:29 -0700

    initial and final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+class SwitchBtn {
+    on: boolean
+    state: string
+    constructor() {
+        this.on = false;
+        this.state = 'off'
+    }
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'SwitchBoard';
+    btnList: SwitchBtn[] = [];
+    numBtns = 10;
+    count = 0;
+    constructor() {
+        while(this.count < this.numBtns) {
+            let newBtn = new SwitchBtn()
+            this.btnList.push(newBtn);
+            this.count++
+        }
+    }
+
+    toggleSwitch(idx): void {
+        if(this.btnList[idx].on == true) {
+            this.btnList[idx].state = 'off';
+            this.btnList[idx].on = false;
+        } else {
+            this.btnList[idx].state = 'on';
+            this.btnList[idx].on = true;
+        }
+
+
+    }
+
+
+
+
+
+
+
 }
